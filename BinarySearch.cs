7e43d360e596d8baded04d98a06d5f51de515c9a
Namrata Chavan <109748579+namratachavan24@users.noreply.github.com>
2022-08-24 00:28:17 +0530

    UC-5 Binary Search

---
+++ BinarySearch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    internal class BinarySearch
+    {
+        public int max;
+        public int min;
+
+        public int binarySearch1(int[] arr, int key)
+        {
+            int minNum = 0;
+            int maxNum = arr.Length - 1;
+
+            while (minNum <= maxNum)
+            {
+                int mid = (minNum + maxNum) / 2;
+                if (key == arr[mid])
+                {
+                    return ++mid;
+                }
+                else if (key < arr[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return 0;
+        }
+    }
+}
